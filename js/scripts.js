ced94f9e2a719dfd54015f6af12eb1491bc6d140
Nina Potrebich <potrebich@gmail.com>
2019-11-01 09:14:21 -0700

    add project transmitting user's input to result box

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
-
-
-
+    $("form#formInput").submit(function(event) {
+        event.preventDefault();
+        const number = parseInt($("#userInput").val());
+        const result = BeepBoop(number);
+        $("#result").text(result);
+        $(".resultBox").show();
+    })
 });
+
+var BeepBoop = (number) => {
+    let resultArray = [];
+    for (let i = 0; i <= number; i++) {
+        resultArray.push(i);
+    }
+    return resultArray;
+}
