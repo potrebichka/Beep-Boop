0621f0feef0358ec61bb60bdf37af624ae118284
Nina Potrebich <potrebich@gmail.com>
2019-11-01 10:41:54 -0700

    add name input

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,32 @@
 $(document).ready(function() {
     $("form#formInput").submit(function(event) {
         event.preventDefault();
-        const number = parseInt($("#userInput").val());
-        const result = BeepBoop(number);
-        $("#result").text(result.join(", "));
-        $(".resultBox").show();
+        $("#result").empty();
+
+        const number = parseInt($("#numberInput").val());
+        const userName = $("#nameInput").val();
+
+        const results = BeepBoop(number, userName);
+        results.forEach((result) => {
+            $("#result").append("<li>" + result + "</li>")
+        });
+        $(".resultBox").slideDown();
     })
 });
 
-var BeepBoop = (number) => {
+var BeepBoop = (number, name) => {
+    if (number < 0 || isNaN(number)){
+        alert("Incorrect input");
+        return;
+    }
+    if (!name) {
+        name = "Dave";
+    }
     let resultArray = [];
     for (let i = 0; i <= number; i++) {
         const indexStr = i.toString();
         if (indexStr.includes(3)) {
-            resultArray.push("I'm sorry, Dave. I'm afraid I can't do that.");
+            resultArray.push("I'm sorry, " + name + ". I'm afraid I can't do that.");
         } else if (indexStr.includes(2)) {
             resultArray.push("Boop");
         } else if (indexStr.includes(1)) {
